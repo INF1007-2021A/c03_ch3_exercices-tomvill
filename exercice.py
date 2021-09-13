86ff5f6d6f46effbed79ee9edf1647419453d183
tomvill <thomas-2.villeneuve@polymtl.ca>
2021-09-13 13:10:24 -0400

    Found the solutions

--- exercice.py
+++ exercice.py
@@ -6,29 +6,37 @@ import math
 
 
 def average(a: float, b: float, c: float) -> float:
-    return 0.0
+    result = (a + b + c)/3
+    return result
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return 0.0
+    angleEnDeg = angle_degs + (angle_mins/60) + (angle_secs/3600)
+    result = math.radians(angleEnDeg)
+    return result
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0
+    result_deg = math.degrees(angle_rads)
+    result_min = result_deg * 60
+    result_sec = result_min * 60
+    return result_deg, result_min, result_sec
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    result = (temperature - 32) * (5/9)
+    return result
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    result = (temperature * (9/5)) + 32
+    return result
 
 
 def main() -> None:
     print(f"Moyenne des nombres 2, 4, 6: {average(2.1, 4.3, 6.5)}")
 
-    print(f"Conversion de 100 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}")
+    print(f"Conversion de 180 degres, 2 minutes et 45 secondes en radians: {to_radians(180, 2, 45)}")
     
     degrees, minutes, seconds = to_degrees(1.0)
     print(f"Conversion de 1 radian en degres: {degrees} degres, {minutes} minutes et {seconds} secondes")
